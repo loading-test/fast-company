561402ccc67bf62aa2d55c637c37bc0a959d8731
Adel Absalyamov <adel.absalyamov@yandex.ru>
2021-10-20 12:17:46 +0300

    editPage component

--- src/components/page/editPage/editPage.jsx
+++ src/components/page/editPage/editPage.jsx
@@ -6,30 +6,49 @@ import TextField from "../../common/form/textField";
 import api from "../../../api";
 import { validator } from "../../../utils/validator";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router";
 
 const EditPage = ({ userId }) => {
-  const [data, setData] = useState({
-    email: "",
-    profession: "",
-    sex: "male",
-    qualities: [],
-  });
+  const history = useHistory();
   const [user, setUser] = useState();
-  const [qualities, setQualities] = useState({});
   const [professions, setProfessions] = useState();
+  const [qualities, setQualities] = useState();
   const [errors, setErrors] = useState({});
-
   useEffect(() => {
-    api.users.getById(userId).then((data) => setUser(data));
-    api.professions.fetchAll().then((data) => setProfessions(data));
-    api.qualities.fetchAll().then((data) => setQualities(data));
+    api.users.getById(userId).then((user) =>
+      setUser({
+        ...user,
+        email: user.email ? user.email : "",
+        sex: user.sex ? user.sex : "other",
+        profession: user.profession._id,
+        qualities: user.qualities.map((qual) => {
+          return { label: qual.name, value: qual._id };
+        }),
+      })
+    );
+    api.professions.fetchAll().then((prof) => setProfessions(prof));
+    api.qualities.fetchAll().then((qual) => setQualities(qual));
   }, []);
 
-  const handleChange = (target) => {
-    setData((prevState) => ({
-      ...prevState,
-      [target.name]: target.value,
-    }));
+  const getProfessionById = (professionId) => {
+    for (const prof in professions) {
+      if (professions[prof]._id === professionId) {
+        return professions[prof];
+      }
+    }
+  };
+
+  const getQualitiesById = (userQualities) => {
+    const newQualities = [];
+    for (const qualUser of userQualities) {
+      for (const qual in qualities) {
+        if (qualUser.value === qualities[qual]._id) {
+          newQualities.push(qualities[qual]);
+        }
+      }
+    }
+    console.log(newQualities);
+    return newQualities;
   };
 
   const validatorConfig = {
@@ -41,89 +60,92 @@ const EditPage = ({ userId }) => {
         message: "Email введен не корректно",
       },
     },
-    password: {
+    name: {
       isRequired: {
-        message: "Пароль обязателен для заполнения",
-      },
-      isCapital: {
-        message: "Пароль должен содержать хотя бы одну заглавную букву",
-      },
-      isContainDigit: {
-        message: "Пароль должен содержать хотя бы одну цифру",
-      },
-      min: {
-        message: "Пароль должен содержать минимум 8 символов",
-        value: 8,
-      },
-    },
-    profession: {
-      isRequired: {
-        message: "Обязательно выберите вашу профессию",
+        message: "Имя обязательно для заполнения",
       },
     },
   };
 
   useEffect(() => {
     validate();
-  }, [data]);
+  }, [user]);
 
   const validate = () => {
-    const errors = validator(data, validatorConfig);
+    const errors = validator(user, validatorConfig);
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
-
-  const isValid = Object.keys(errors).length === 0;
+  const handleChange = (target) => {
+    console.log(target.name, target.value);
+    setUser((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-     api.users.update(userId, data)
-    console.log(data);
+    api.users
+      .update(userId, {
+        ...user,
+        profession: getProfessionById(user.profession),
+        qualities: getQualitiesById(user.qualities),
+      })
+      .then((user) => history.replace(`/users/${user._id}`));
   };
+
+  const isValid = Object.keys(errors).length === 0;
 
   if (user) {
     return (
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-6 offset-md-3 shadow p-4">
-            <h3 className="mb-4">{user.name}</h3>
+          <div className="col-md-6 offset-md-3 shadow p-5">
             <form onSubmit={handleSubmit}>
+              <TextField
+                label="Имя"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+                error={errors.name}
+              />
               <TextField
                 label="Электронная почта"
                 name="email"
-                value={data.email}
+                value={user.email}
                 onChange={handleChange}
                 error={errors.email}
               />
               <SelectField
-                label="Выберите вашу профессию"
-                onChange={handleChange}
+                label="Выберите свою профессию"
+                value={user.profession}
                 options={professions}
-                defaultOption="Choose..."
-                error={errors.profession}
-                value={data.profession}
-                name="profession"
+                onChange={handleChange}
+                name="profission"
               />
+
               <RadioField
                 options={[
                   { name: "Male", value: "male" },
                   { name: "Female", value: "female" },
                   { name: "Other", value: "other" },
                 ]}
-                value={data.sex}
+                value={user.sex}
                 name="sex"
                 onChange={handleChange}
                 label="Выберите Ваш пол"
               />
               <MultiSelectField
                 options={qualities}
-                onChange={handleChange}
                 name="qualities"
+                onChange={handleChange}
                 label="Выберите Ваши качества"
+                value={user.qualities}
               />
               <button
-                type="submit"
                 disabled={!isValid}
+                type="submit"
                 className="btn btn-primary w-100 mx-auto"
               >
                 Обновить
@@ -133,9 +155,8 @@ const EditPage = ({ userId }) => {
         </div>
       </div>
     );
-  } else {
-    return <h1>Loading</h1>
   }
+  return "Loading...";
 };
 EditPage.propTypes = {
   userId: PropTypes.string,
